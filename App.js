b13433934b83a008a9692377752890f5dbeb5467
Binay Kumar Das <85559080+binaykumardas@users.noreply.github.com>
2024-01-10 23:04:26 +0530

    string reverse

--- App.js
+++ App.js
@@ -1,39 +1,34 @@
-// using for loop
-let arr = [1, 6, 8, 3, 3, 1, 1, 8, 10];
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
-function checkDuplicateElement(arr) {
-  let duplicateElemnt = [];
-  for (const element1 in arr) {
-    for (const element2 in arr) {
-      if (element1 === element2) {
-        continue;
-      } else {
-        if (arr[element1] === arr[element2]) {
-          duplicateElemnt.push(arr[element1]);
-        }
-      }
+// Input: "hello"
+// Output: "holle"
+
+// Using Two Pointer approch(in place reversal)
+let str = 'AEIOU';
+let newStr = '';
+function reverseString(str) {
+  let arr = str.split('');
+  let i=0;
+  let j = str.length - 1;
+  while(i < j) {
+    // while i is a vowel and j also a vowel then i++ and j-- and do swapping
+    // while i is a vowel but j is not a vowel then i will be not increase and j will j--
+    // while i is not  vowel but j is a vowel then i++ and j will not decrease
+    // while i is not  vowel but j is not a vowel then i++ and j--
+    const vowel = 'aeiouAEIOU';
+    if(vowel.indexOf(str.charAt(i)) !== -1 && vowel.indexOf(str.charAt(j)) !== -1) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+      j--;
+    } else if(vowel.indexOf(str.charAt(i)) === -1 && vowel.indexOf(str.charAt(j)) !== -1) {
+      i++;
+    } else if(vowel.indexOf(str.charAt(i)) !== -1 && vowel.indexOf(str.charAt(j)) === -1) {
+      j--;
+    } else {
+      i++;
+      j--;
     }
   }
-  return [...new Set(duplicateElemnt)];
+  return arr.join('');
 }
-console.log("===>", checkDuplicateElement(arr));
-
-
-// Using set method
-function checkDuplicateElement(arr) {
-    const newSet = new Set();
-    const duplicateSet = new Set();
-    for (let i = 0; i < arr.length; i++) {
-        if (!newSet.has(arr[i])) {
-            newSet.add(arr[i]);
-        } else {
-            duplicateSet.add(arr[i]);
-        }
-    }
-   // return duplicateSet; // if you want result inside an object
-    return [...new Set(duplicateSet)]; // if you want result inside an array
-}
-console.log("===>", checkDuplicateElement(arr));
- 
-
-    
+console.log('reverse str=>',reverseString(str));
